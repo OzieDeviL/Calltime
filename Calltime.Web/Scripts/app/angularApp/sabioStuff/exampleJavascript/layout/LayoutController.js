bbf6b9e635276ff622c5c850ecb1c8b6687ce683
DavidLaughlin(WinVM) <davidclaughlin@gmail.com>
2017-04-12 20:26:39 -0700

    Added Angular and corrected JSON script in WebApiConfig for casing

---
+++ Calltime.Web/Scripts/app/angularApp/sabioStuff/exampleJavascript/layout/LayoutController.js
@@ -0,0 +1,27 @@
+﻿(function () {
+
+    'use strict';
+    console.log('creating the Layout Controller...');
+    angular.module(example.APPNAME).controller('layoutController', LayoutController);
+    LayoutController.$inject = ['$scope'];
+
+    function LayoutController($scope) {
+
+        console.log('entering LayoutController function creation...');
+        var vm = this;
+        vm.$scope = $scope;
+        vm.msg = 'Layout Controller is available!';
+        vm.displayBodyContent = true;
+
+        render();
+        function render() {
+
+            console.log('Starting render method of the LayoutController...');
+            vm.displayBodyContent = true;
+            console.log('Layout is ready!');
+
+        };
+
+    };
+
+})();
---
+++ Calltime.Web/Scripts/app/angularApp/sabioStuff/sabio.module.xeditable.js
@@ -0,0 +1,17 @@
+﻿
+
+(function ($sabio) {
+
+
+    $sabio.moduleOptions.extraModuleDependencies.push("xeditable");
+    console.log('hi');
+
+    $sabio.moduleOptions.runners.push(exditableOptionRunner);
+
+    function exditableOptionRunner(editableOptions) {
+        editableOptions.theme = 'bs3'; // bootstrap3 theme. Can be also 'bs2', 'default'
+    }
+
+
+}(sabio))
+
